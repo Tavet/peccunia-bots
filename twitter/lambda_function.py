0fe1c8eb9f37befe19e291d4a7a2276afcfa4801
Breyner <mon2bdr@gmail.com>
2021-04-28 23:32:10 -0500

    Update lambda function to decrypt the environment variables

--- twitter/lambda_function.py
+++ twitter/lambda_function.py
@@ -1,12 +1,28 @@
 import os
 import tweepy
+import boto3
+from base64 import b64decode
+
+kms = boto3.client('kms')
 
 
 def twitter_api():
-    access_token = os.environ.get('ACCESS_TOKEN')
-    access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
-    consumer_key = os.environ.get('CONSUMER_KEY')
-    consumer_secret = os.environ.get('CONSUMER_SECRET')
+    access_token = kms.decrypt(
+        CiphertextBlob=b64decode(os.environ['ACCESS_TOKEN']),
+        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+    )['Plaintext'].decode('utf-8')
+    access_token_secret = kms.decrypt(
+        CiphertextBlob=b64decode(os.environ['ACCESS_TOKEN_SECRET']),
+        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+    )['Plaintext'].decode('utf-8')
+    consumer_key = kms.decrypt(
+        CiphertextBlob=b64decode(os.environ['CONSUMER_KEY']),
+        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+    )['Plaintext'].decode('utf-8')
+    consumer_secret = kms.decrypt(
+        CiphertextBlob=b64decode(os.environ['CONSUMER_SECRET']),
+        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+    )['Plaintext'].decode('utf-8')
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -14,4 +30,6 @@ def twitter_api():
 
 
 def lambda_handler(event, context):
-    return 'Works!!!'
+    twitter_api().update_status("#CRYPTOS Peccunia Rules")
+    return 'Tweet publicado'
+
